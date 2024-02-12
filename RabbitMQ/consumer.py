ad064ff51d254bd22f38eedd5d00dd63e7196d57
Bidyut <bidyut@divine-id.net>
2024-02-12 16:35:32 +0600

    ..

--- RabbitMQ/consumer.py
+++ RabbitMQ/consumer.py
@@ -2,18 +2,29 @@ import pika
 
 def on_message_received(ch, method, properties, body):
     print(f'Received message {body}')
+    
+def callback(ch, method, properties, body):
+    print("Received message:", body.decode())
 
-credentials = pika.PlainCredentials('guest', 'guest')
-connection_parameter =  pika.ConnectionParameters('localhost', 5672, '/', credentials)
+credentials = pika.PlainCredentials('bidyut', '1234')
+connection_parameter =  pika.ConnectionParameters('192.168.10.177', 5672, '/', credentials)
 
 connection = pika.BlockingConnection(connection_parameter)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='fttx_boxs')
+# get message by que
+channel.queue_declare(queue='tofa_1234')
+channel.basic_consume(queue='tofa_1234', auto_ack=True, on_message_callback=on_message_received,)
 
-channel.basic_consume(queue='fttx_boxs', auto_ack=True, on_message_callback=on_message_received)
+# get message by exchange
+# exchange_name = 'tofa_1234'
+# channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+# result = channel.queue_declare(queue='', exclusive=True)
+# queue_name = result.method.queue
+# channel.queue_bind(exchange=exchange_name, queue=queue_name)
+# channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
 
 print('Starting consuming')
-
 channel.start_consuming()
--- RabbitMQ/producer.py
+++ RabbitMQ/producer.py
@@ -1,20 +1,18 @@
 import pika
 import time
-credentials = pika.PlainCredentials('guest', 'guest')
+credentials = pika.PlainCredentials('bidyut', '1234')
 connection_parameter =  pika.ConnectionParameters('localhost', 5672, '/', credentials)
 
 connection = pika.BlockingConnection(connection_parameter)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='fttx_boxs')
+channel.queue_declare(queue='tofa_1234')
 
-for step in range(1, 20):
+for step in range(0, 5):
     message = f'(Step:{step}) Hello from fttx_box!'
-    channel.basic_publish(exchange='', routing_key='fttx_boxs', body=message)
+    channel.basic_publish(exchange='', routing_key='tofa_1234', body=message)
     time.sleep(2)
     print('Sent message', message, step)
-
-
 
 connection.close()
