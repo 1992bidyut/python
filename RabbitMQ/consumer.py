1552744017239c89c6880d9cccc30066292b8064
Bidyut <bidyut@divine-id.net>
2024-02-12 11:20:21 +0600

    .

--- RabbitMQ/consumer.py
+++ RabbitMQ/consumer.py
@@ -3,15 +3,16 @@ import pika
 def on_message_received(ch, method, properties, body):
     print(f'Received message {body}')
 
-connection_parameter =  pika.ConnectionParameters('localhost')
+credentials = pika.PlainCredentials('guest', 'guest')
+connection_parameter =  pika.ConnectionParameters('localhost', 5672, '/', credentials)
 
 connection = pika.BlockingConnection(connection_parameter)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='fttx_box')
+channel.queue_declare(queue='fttx_boxs')
 
-channel.basic_consume(queue='fttx_box', auto_ack=True, on_message_callback=on_message_received)
+channel.basic_consume(queue='fttx_boxs', auto_ack=True, on_message_callback=on_message_received)
 
 print('Starting consuming')
 
--- RabbitMQ/producer.py
+++ RabbitMQ/producer.py
@@ -1,17 +1,17 @@
 import pika
 import time
-
-connection_parameter =  pika.ConnectionParameters('localhost')
+credentials = pika.PlainCredentials('guest', 'guest')
+connection_parameter =  pika.ConnectionParameters('localhost', 5672, '/', credentials)
 
 connection = pika.BlockingConnection(connection_parameter)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='fttx_box')
+channel.queue_declare(queue='fttx_boxs')
 
 for step in range(1, 20):
     message = f'(Step:{step}) Hello from fttx_box!'
-    channel.basic_publish(exchange='', routing_key='fttx_box', body=message)
+    channel.basic_publish(exchange='', routing_key='fttx_boxs', body=message)
     time.sleep(2)
     print('Sent message', message, step)
 
